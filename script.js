78874b83364aa17502d5a2286dbdaa1f56c6282d
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2024-10-20 18:43:53 +0200

    debugging using the console to confirm if all user input is in lowercases

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ function validateSyntax()
     //Convert the input to lowercase
     input = input.toLowerCase();
 
+    //Debugging using the console 
+    console.log("User input: ", input);
+
     //Validation
     let regex = /^pet_\d{4}[a-z0-9]+$/;
 
     //Comparison
     result = regex.test(input)? "Valid Syntax" : "Invalid Syntax";
 
-    
+  
     document.getElementById('result').innerText = result;
 }
 
