f2272543d98113aa37f96cfe05a5a20e89d240e1
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2024-10-20 18:39:40 +0200

    Comparing the regex with the user input. A shorthand if else is used. Which takes up less space compared to the traditional else if statement.

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ function validateSyntax()
     //Validation
     let regex = /^pet_\d{4}[a-z0-9]+$/;
 
+    //Comparison
+    result = regex.test(input)? "Valid Syntax" : "Invalid Syntax";
 
-    ocument.getElementById('result').innerText = result;
+    
+    document.getElementById('result').innerText = result;
 }
 
 
